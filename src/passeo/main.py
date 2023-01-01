10aa1ac42eb2ec33d147208db69e10610a34675e
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2022-12-31 18:59:02 -0600

    Update main.py

--- src/passeo/main.py
+++ src/passeo/main.py
@@ -1,6 +1,4 @@
 import click
-import secrets
-import string
 from passeo import passeo
 
 @click.group()
@@ -25,54 +23,13 @@ def cli():
 @click.argument("save", type=bool)
 def generate(generate, length, numbers, symbols, uppercase, lowercase, space, save):
     try:
-        password = ''
-
-        if numbers is True:
-            password += secrets.choice(string.digits)
-
-        if symbols is True:
-            password += secrets.choice(string.punctuation)
-
-
-        if uppercase is True:
-            password += secrets.choice(string.ascii_uppercase)
-
-        if lowercase is True:
-            password += secrets.choice(string.ascii_lowercase)
-
-        if space is True:
-            password += password.join (' ')
-
-        if save is True:
-            with open("passeo_passwords.txt", "a") as f:
-                f.write(password + "\n")
-
-        elif numbers is False:
-            pass
-
-        elif symbols is False:
-            pass
-
-        elif uppercase is False:
-            pass
-
-        elif lowercase is False:
-           pass
-
-        elif space is False:
-            pass
-
-        elif save is False:
-            pass
-
 
         if click.confirm("Do you want to generate a password? Details: \n Length: " + str(length) + "\n Numbers: " + str(numbers) +"\n Symbols: " + str(symbols) + "\n Uppercase: " + str(uppercase) + "\n Lowercase: " + str(lowercase) + "\n Space: " + str(space) + "\n Save: " + str(save)):
-            FinalPassword = ''.join(secrets.choice(password) for i in range(length))
+            FinalPassword = ''.join(passeo().generate(length=length, numbers=numbers, symbols=symbols, uppercase=uppercase, lowercase=lowercase, space=space, save=save))
             click.echo(FinalPassword)
 
     except Exception:
             click.echo(Exception)
-
 
 @cli.command()
 @click.option(
@@ -88,6 +45,37 @@ def strengthcheck(strengthcheck, password):
             click.echo(passeo().strengthcheck(password))
     except Exception:
         click.echo(Exception)
+
+@cli.command()
+@click.option(
+    "-qg",
+    "--quickgenerate",
+    "--qg",
+    "--quickgen",
+    "--qgen",
+    "--quick",
+    "-quickgen",
+    is_flag=True,
+    help="Generate a password.",
+)
+@click.argument("length", type=int)
+@click.argument("save", type=bool)
+@click.argument("bulk", type=int)
+def quickgenerate(quickgenerate, length, save, bulk):
+    password = passeo().quickgenerate(length, save, bulk)
+    click.confirm("Do you want to quickgenerate a password? Details: \n Length: " + str(length) + "\n Save: " + str(save) + "\n Bulk: " + str(bulk))
+    if save is True:
+        with open("passeo_quickgen_passwords.txt", "a") as f:
+            f.write(password + "\n")
+
+    if bulk > 1:
+         with open("passeo_quickgen_bulk_passwords.txt", "a") as f:
+            for i in range(bulk):
+                 bulk_password = passeo().quickgenerate(length, save, bulk)
+                 f.write(bulk_password + "\n")
+                 click.echo(bulk_password)
+
+    click.echo(password)
 
 
 
