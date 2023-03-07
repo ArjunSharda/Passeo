2115485be5d4f205f042acf93f3124a3866d048a
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2023-03-07 09:35:44 -0600

    Update generate.py
    change examples to adjust to breaking change

--- examples/generate.py
+++ examples/generate.py
@@ -1,3 +1,3 @@
 from passeo import passeo
 
-print(passeo().generate(10, numbers=True, symbols=True, uppercase=True, lowercase=False, space=True, save=True))
+print(passeo.generate(10, numbers=True, symbols=True, uppercase=True, lowercase=False, space=True, save=True))
